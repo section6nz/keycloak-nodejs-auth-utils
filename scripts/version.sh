67c587d094bc5ccdde5cbd70b63f15287790bf1d
Bruno Oliveira <bruno@abstractj.org>
2017-08-18 10:12:53 -0300

    Fix Travis script to start up Keycloak 2.5.x
    Signed-off-by: Bruno Oliveira <bruno@abstractj.org>

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-export VERSION=`curl -s https://repo1.maven.org/maven2/org/keycloak/keycloak-server-dist/ | sed -e 's/<[^>]*>//g' | grep -i final | cut -d '/' -f1 | tail -n1`
+export VERSION=`curl -s https://repo1.maven.org/maven2/org/keycloak/keycloak-server-dist/ | sed -e 's/<[^>]*>//g' |
+grep -i 2.[0-9].[0-9].final | cut -d '/' -f1 | tail -n1`
 export KEYCLOAK="keycloak-${VERSION}"
